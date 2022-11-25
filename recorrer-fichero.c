f3a3f963f5c86e74dcad6e2ed023291b103c0b2a
enriqueM02 <00016522@uca.edu.sv>
2022-11-25 00:46:17 -0600

    subi juego y fichero

---
+++ recorrer-fichero.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    int cont = 0; 
+    char carater; 
+
+    file *fp
+
+    while (1)
+    {
+
+    caracter = fgetc (fp);
+
+     if(caracter = '\n')
+        {
+        cont++;
+        }
+    }
+
+    /* if (caracter == EOF0)
+    {
+        printf("el documento tiene %i lineas\h", cont)
+    }
+    */
+                
+
+
+
+
+
+
+
+    return 0;
+}
